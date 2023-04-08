7895b4044f8c90eb40c3c6ff7f27b400f5b25663
root <root@DESKTOP-59ONIOG.localdomain>
2023-04-08 14:40:44 -0400

    Add flask backend

--- frontend/src/render.js
+++ frontend/src/render.js
@@ -1,4 +1,28 @@
 const { ipcRenderer } = require('electron');
+const { axios } = require("axois");
+
+const SERVER_URL = "http://127.0.0.1:5000";
+(() => {
+  axios
+    .get(`${SERVER_URL}/`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+})();
+
+const fileRequest = (requestObject) => {
+  axios
+    .post(`${SERVER_URL}/file`, requestObject)
+    .then((response) => {
+      console.log("Server response", response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+}
 
 const selectDirectoryButton = document.getElementById('select-dir');
 const fileList = document.getElementById('file-list');
@@ -8,30 +32,35 @@ selectDirectoryButton.addEventListener('click', () => {
 });
 
 ipcRenderer.on('selected-directory', (event, directoryPath) => {
-    // Clear the current file list
-    fileList.innerHTML = '';
-  
-    // Get the directory contents
-    const fs = require('fs');
-  
-    recListFiles(directoryPath, fileList, fs);
+  // Clear the current file list
+  fileList.innerHTML = '';
+
+  // Get the directory contents
+  const fs = require('fs');
+
+  const fileRequestObject = {
+    path: directoryPath
+  }
+  fileRequest(fileRequestObject);
+
+  recListFiles(directoryPath, fileList, fs);
 });
 
-function recListFiles(path, fileList, fs){
-    console.log(path);
-    const itemElement = document.createElement('li');
-    if(!fs.statSync(path).isDirectory()){
-        // Base case (file)
-        itemElement.textContent = `📃 ${path.split("/").slice(-1)}`;
-        itemElement.classList.add('indent-2');
-        fileList.appendChild(itemElement);
-    }else{
-        const dirContents = fs.readdirSync(path);
-        itemElement.textContent = `📁 ${path.split("\\").slice(-1)}`
-        fileList.appendChild(itemElement);
-        // Recursive step
-        for (const item of dirContents) {
-            recListFiles(`${path}/${item}`, fileList, fs);
-        }
+function recListFiles(path, fileList, fs) {
+  console.log(path);
+  const itemElement = document.createElement('li');
+  if (!fs.statSync(path).isDirectory()) {
+    // Base case (file)
+    itemElement.textContent = `📃 ${path.split("/").slice(-1)}`;
+    itemElement.classList.add('indent-2');
+    fileList.appendChild(itemElement);
+  } else {
+    const dirContents = fs.readdirSync(path);
+    itemElement.textContent = `📁 ${path.split("\\").slice(-1)}`
+    fileList.appendChild(itemElement);
+    // Recursive step
+    for (const item of dirContents) {
+      recListFiles(`${path}/${item}`, fileList, fs);
     }
+  }
 }
