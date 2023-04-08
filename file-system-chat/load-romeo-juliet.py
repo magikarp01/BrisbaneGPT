7dfaf3c13ed7ec510b202a44d112ebdaf6a4bd6b
magikarp01 <philliphguo@gmail.com>
2023-04-07 22:30:54 -0400

    Making embeddings

--- file-system-chat/load-romeo-juliet.py
+++ file-system-chat/load-romeo-juliet.py
@@ -17,16 +17,3 @@ print('Python:', platform.python_version())
 os.environ["OPENAI_API_KEY"] = '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'
 persist_directory="./embeddings"
 
-def get_gutenberg(url):
-    loader = GutenbergLoader(url)
-    data = loader.load()
-    return data
-
-romeoandjuliet_data = get_gutenberg('https://www.gutenberg.org/cache/epub/1513/pg1513.txt')
-
-text_splitter = TokenTextSplitter(chunk_size=100, chunk_overlap=0)
-romeoandjuliet_doc = [text_splitter.split_documents(romeoandjuliet_data)[0]]
-
-embeddings = OpenAIEmbeddings()
-vectordb = Chroma.from_documents(romeoandjuliet_doc, embeddings, persist_directory=persist_directory)
-vectordb.persist()
