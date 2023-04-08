81ef5a8963c0b8bb0f35103e8b144fb2d84de2ec
Aaquib <aaquibahm@gmail.com>
2023-04-08 15:01:09 -0400

    Added messaging feature

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,8 +23,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
    mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: false,
--- frontend/src/render.js
+++ frontend/src/render.js
@@ -1,19 +1,24 @@
 const { ipcRenderer } = require('electron');
 
 const selectDirectoryButton = document.getElementById('select-dir');
+const clearDirectoryButton = document.getElementById('clear-dir');
+const chatSubmitButton = document.getElementById('chat-submit');
+
 const fileList = document.getElementById('file-list');
 
 selectDirectoryButton.addEventListener('click', () => {
   ipcRenderer.send('open-directory-dialog');
 });
 
-ipcRenderer.on('selected-directory', (event, directoryPath) => {
-    // Clear the current file list
+clearDirectoryButton.addEventListener('click', () => {
+    const fileList = document.getElementById('file-list');
     fileList.innerHTML = '';
-  
+});
+
+ipcRenderer.on('selected-directory', (event, directoryPath) => {  
     // Get the directory contents
     const fs = require('fs');
-  
+    
     recListFiles(directoryPath, fileList, fs);
 });
 
@@ -28,6 +33,7 @@ function recListFiles(path, fileList, fs){
     }else{
         const dirContents = fs.readdirSync(path);
         itemElement.textContent = `📁 ${path.split("\\").slice(-1)}`
+
         fileList.appendChild(itemElement);
         // Recursive step
         for (const item of dirContents) {
